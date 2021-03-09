a99b096c5c7f95ad3461ccc3e758d1d9571ffb49
LamNik324 <89999809989@yandex.ru>
2021-03-09 17:19:30 +0300

    last

--- src/client/app/public/components/spinner/Spinner.tsx
+++ src/client/app/public/components/spinner/Spinner.tsx
@@ -5,6 +5,7 @@ export const Spinner = () => {
     return (
         <div className="Spinner">
             <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+            {/* <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div> */}
         </div>
     )
 }
--- src/client/app/public/modals/login/Login.tsx
+++ src/client/app/public/modals/login/Login.tsx
@@ -1,43 +1,76 @@
 import * as React from 'react';
 
+import { Spinner } from '../../components/spinner/Spinner';
 import { img_next, img_activeEye, img_disableEye } from '../../../../resources/images';
+import { CONNECTION } from '../../../../Connection';
+import { LoginRequest } from '../../../../generated/proto.web';
 
-interface IPassword {
+interface PasswordViewModeModel {
     img : string;
     type : 'text' | 'password';
 }
 
 export const Login = () => {
 
-    const [password, setPassword] = React.useState<IPassword>({
+    const [passwordViewMode, setPasswordViewMode] = React.useState<PasswordViewModeModel>({
         img : img_activeEye,
         type : 'password'
     })
 
+    const email = React.useRef<HTMLInputElement>()
+    const password = React.useRef<HTMLInputElement>()
+
+    const [inProgress, setInProgress] = React.useState<boolean>(false);
+
     const handleShowPassword = () => {
-        if (password.type === 'password') {
-            setPassword({
+        if (passwordViewMode.type === 'password') {
+            setPasswordViewMode(prev => ({
+                ...prev,
                 img : img_disableEye,
                 type : 'text'
-            })
+            }))
         } else {
-            setPassword({
+            setPasswordViewMode(prev =>({
+                ...prev,
                 img : img_activeEye,
                 type : 'password'
-            })
+            }))
         }
     }
+
+    const handleLogin = () => {
+        setInProgress(prev => prev = true);
+        
+        const subscription = CONNECTION.login(createLoginRequest())
+            
+
+            
+    }
+
+    const showInProgress = () => {
+        if (inProgress) {
+            return <Spinner/>
+        }
+        else {
+            return <img onClick={handleLogin} src={img_next} className='button-next' alt="Next"/>
+        }
+    }
+
+    const createLoginRequest = () : LoginRequest => ({
+        email : email.current.value,
+        password : password.current.value
+    })
 
     return (
         <div className="Login" onClick={(e) => e.stopPropagation ()}>
             <div className="title">Войти в личный кабинет</div>
             <div className="inputs-block">
-                <input required name='email' className='input-email' placeholder='Эл.почта' type="text"/>
-                <input required name='password' className='input-password' placeholder='Пароль' type={password.type}/>
+                <input ref={email} disabled={inProgress} required name='email' className='input-email' placeholder='Эл.почта' type="text"/>
+                <input ref={password} disabled={inProgress} required name='password' className='input-password' placeholder='Пароль' type={passwordViewMode.type}/>
                 <div onClick={handleShowPassword} className="img-password">
-                    <img src={password.img} alt="Eye"/>
+                    <img src={passwordViewMode.img} alt="Eye"/>
                 </div>
-                <img src={img_next} className='button-next' alt="Next"/>
+                {showInProgress()}
                 <div className="forgot-password">Восстановить пароль</div>
             </div>
         </div>
