2bd382b285160f68e4254c7e3507fbd6a04aa0fd
imd <mdi@glonassmobile.com>
2021-06-09 15:46:29 +0300

    chore: grpc

--- next.config.js
+++ next.config.js
@@ -4,5 +4,26 @@ module.exports = withImages({
     distDir: 'build/.next',
     future: {
         webpack5: true
-    }
+    },
+    webpack: (config, { dev }) => {
+
+        var path = ''
+        
+        if (dev) {
+            path = "pages/"
+        }
+
+        config.module.rules.push(
+          {
+            test: [/\.proto$/],
+            loader: require.resolve('file-loader'),
+            options: {
+              name: './' + path + '[name].proto',
+              emitFile: true,
+              useRelativePath : true
+            }
+          }
+        )
+        return config
+      }
 })
